a98d04387158e6c0b9e5fe185c99c24abc28f112
ShreyWey <shreyansh.chhalotra@gmail.com>
2023-12-26 16:06:32 -0600

    validate funct

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 from random import choice
+import string
 
 def select_word():
     with open ("words.txt", mode="r") as words:
@@ -10,3 +11,11 @@ def get_player_input(guessed_letters):
         player_input = input("Guess a letter: ").lower()
         if _validate_input(player_input, guessed_letters):
             return player_input
+
+def _validate_input(player_input, guessed_letters):
+    return (
+        len(player_input) == 1
+        and player_input in string.ascii_lowercase
+        and player_input not in guessed_letters
+    )
+
